89d2b5365b7486a372bb03ffbdfc2109ee3087b9
Luca Fabri <luca.fabri1999@gmail.com>
2024-01-15 17:25:36 +0100

    feat: replay buffer controller's data batching

--- src/main/main.py
+++ src/main/main.py
@@ -1,18 +1,26 @@
-from src.main.controllers.buffer_controller.replay_buffer_controller import ReplayBufferController
+from src.main.controllers.buffer_controller.replay_buffer_controller import (
+    ReplayBufferController,
+)
 
 from src.main.controllers.learner_controller.learner import Learner
 
-if __name__ == '__main__':
-
+if __name__ == "__main__":
     batch_size = 64
-    num_states = 14
+    num_states = 2
     num_actions = 2
-    num_predators = 10
-    num_preys = 10
+    num_agents = 10
 
     learner = Learner(
-        replay_buffer_controller=ReplayBufferController(batch_size),
-        num_agents=num_predators + num_preys,
+        replay_buffer_controller=ReplayBufferController(
+            batch_size,
+            num_states=num_states,
+            num_actions=num_actions,
+            num_agents=num_agents,
+        ),
+        num_agents=num_agents,
         num_states=num_states,
-        num_actions=num_actions
+        num_actions=num_actions,
     )
+
+    while True:
+        learner.update()
