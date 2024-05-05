cd02cd7092d160fa36f7bfbfc1f23d2e7e45ca2c
Luca Fabri <luca.fabri1999@gmail.com>
2024-05-05 10:19:42 +0200

    fix: send data to sim

--- src/main/controllers/actor_sender_controller/actor_sender_controller.py
+++ src/main/controllers/actor_sender_controller/actor_sender_controller.py
@@ -1,30 +1,33 @@
+
+import keras
 import pika
 import os
 
 
 class ActorSenderController:
-    def __init__(self, broker_host: str):
+    def __init__(self, broker_host: str, routing_key: str):
         self.connection = pika.BlockingConnection(
             pika.ConnectionParameters(broker_host)
         )
         self.channel = self.connection.channel()
         self.path = os.path.dirname(os.path.abspath(__file__))
+        self.routing_key = routing_key
         # Declare a topic exchange named 'topic_exchange'
         self.channel.exchange_declare(exchange="topic_exchange", exchange_type="topic")
 
-    def _send(self, routing_key, actor_model):
-
+    def _send(self, routing_key, actor_model: keras.Model):
         file_path = f"{self.path}/resources/{routing_key}.keras"
         actor_model.save(file_path)
 
         with open(file_path, 'rb') as actor_model_file:
-            # Publish the message to the topic exchange with the specified routing key
-            self.channel.basic_publish(
-                exchange="topic_exchange", routing_key=routing_key, body=actor_model_file.read()
-            )
+            actor_model_bytes = actor_model_file.read()
 
-        print(f" [x] Sent '{actor_model}' with routing key '{routing_key}'")
+        self.channel.basic_publish(
+            exchange="topic_exchange", routing_key=routing_key,
+            body=actor_model_bytes
+        )
 
     def send_actors(self, actor_models):
-        for i in range(len(actor_models)):
-            self._send(f"actor-model-{i}", actor_models[i])
+        print("Updating actor policies")
+        for i in range(1):
+            self._send(self.routing_key, actor_models[i])
